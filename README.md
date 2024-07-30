6cd4c695252151f8599d122885779a2dbd276f94
plague <viniciuslucianocardoso@gmail.com>
2024-07-30 20:29:57 -0300

    fix: removed the css parser and cleaned the code a bit

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Obsidian Stylist
 
-*Stylist* is a plugin for Obsidian that allows to add classes and styles on markdown.
+_Stylist_ is a plugin for Obsidian that allows to add classes and styles on markdown.
 
+> [!note]
+> This is a fork of the original plugin made by [@ixth](https://github.com/ixth).
+> The only thing I did was remove the css parser, because it limited the user so be more aware using
+> it on generic selector. I do not pretend on updating it further.
 
-> **Note**
+> [!warning]
 > This plugin doesn't work in Live Preview mode.
-
 
 ## Examples
 
@@ -23,8 +26,8 @@ h2 {
 
 Result:
 
-![Style tag render](./style-dark.png#gh-dark-mode-only)
-![Style tag render](./style-light.png#gh-light-mode-only)
+![Style tag render](.assets/style-dark.png#gh-dark-mode-only)
+![Style tag render](.assets/style-light.png#gh-light-mode-only)
 
 > **Note**
 > All styles are processed to prevent messing up the rest of the Obsidian app.
@@ -41,23 +44,23 @@ Or if you want certain blocks on your page to use multi-column layout, you can j
 ## What also floats in water?
 
 %% Class will be added not to the list element itself,
-    but to section container, so you should write styles respectively %%
+but to section container, so you should write styles respectively %%
 
 `classname:multicol-3`
 
-* Bread
-* Apples
-* Very small rocks
-* Cider
-* Grape gravy
-* Cherries
-* Mum
-* Churches
-* Lead
-* A duck
+-   Bread
+-   Apples
+-   Very small rocks
+-   Cider
+-   Grape gravy
+-   Cherries
+-   Mum
+-   Churches
+-   Lead
+-   A duck
 ````
 
 Result:
 
-![Class render](./class-dark.png#gh-dark-mode-only)
-![Class render](./class-light.png#gh-light-mode-only)
+![Class render](.assets/class-dark.png#gh-dark-mode-only)
+![Class render](.assets/class-light.png#gh-light-mode-only)
--- main.ts
+++ main.ts
@@ -1,54 +1,39 @@
-import { hasPseudoElements, parseRulesets, parseSelector, splitPseudoElements } from 'not-a-real-css-parser';
-import { Plugin } from 'obsidian';
+import { Plugin } from "obsidian";
 
-const CLASSNAME_TAG = 'classname:';
-
-const makeScopedStyles = (rootSelector: string, source: string): string =>
-    parseRulesets(source).map(({ selector, declarations }) => {
-        if (hasPseudoElements(selector)) {
-            return parseSelector(selector).map((selector) => {
-                const [selectorHead, pseudoElement] = splitPseudoElements(selector);
-                return `${rootSelector} :is(${selectorHead})${pseudoElement ?? ''} {${declarations}}`;
-            })
-            .join(',');
-        }
-        return `${rootSelector} :is(${selector}) {${declarations}}`;
-    })
-    .join('\n');
+const CLASSNAME_TAG = "classname:";
 
 export default class extends Plugin {
-    private lastClassName: string | undefined;
+	onload() {
+		// Render 'style' code blocks
+		this.registerMarkdownCodeBlockProcessor("style", (source, element) => {
+			element.createEl("style", {
+				text: source,
+			});
+		});
 
-    onload() {
-        this.registerMarkdownCodeBlockProcessor('style', (source, element) => {
-            element.createEl('style', {
-                text: makeScopedStyles('.markdown-preview-view', source)
-            });
-        });
+		// Insert the class in
+		let className: string | null = null;
 
-        this.registerMarkdownPostProcessor((element) => {
-            if (element.hasClass('next-class-skip')) {
-                return;
-            }
+		this.registerMarkdownPostProcessor((element) => {
+			// skips elements with the 'stylist-skip' class
+			// if (element.hasClass("stylist-skip")) return;
 
-            if (this.lastClassName !== undefined) {
-                element.classList.add(...this.lastClassName.split(/\s+/));
-                this.lastClassName = undefined;
-            }
+			// adds the class to the element and clears the class string
+			if (className) {
+				element.classList.add(...className.split(/\s+/));
+				className = null;
+			}
 
-            const codeElements = element.querySelectorAll('code');
-
-            const classElement = [...codeElements].find(
-                (element) => element.innerText.trim().startsWith(CLASSNAME_TAG)
-            );
-
-            if (classElement) {
-                this.lastClassName = classElement.innerText.slice(CLASSNAME_TAG.length);
-                if (classElement.parentElement?.childElementCount === 1) {
-                    classElement.parentElement.remove();
-                }
-                element.classList.add('next-class-skip');
-            }
-        });
-    }
+			// gets the code blocks that match the CLASSNAME_TAG
+			const classBlock = [...element.querySelectorAll("code")].find(
+				(codeEl) => codeEl.innerText.trim().startsWith(CLASSNAME_TAG),
+			);
+			if (classBlock) {
+				className = classBlock.innerText
+					.replace(CLASSNAME_TAG, "")
+					.trim(); // sets the className to be set in the next element
+				classBlock.parentElement?.remove(); // removes the classBlock element from render
+			}
+		});
+	}
 }
